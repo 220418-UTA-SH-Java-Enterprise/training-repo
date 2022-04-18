c1d86fbb529b9a065f326cd8d58d4fdf864347f3
aaknox <azhya.knox@gmail.com>
2022-04-18 17:09:59 -0500

    adding bad-to-good conditional statement demo

--- demos/1 - Java/HelloWorld.java
+++ demos/1 - Java/HelloWorld.java
@@ -4,13 +4,24 @@ public class HelloWorld {
     //method = function = useds to execute behavior in our objects
     //parameter = arguments/inputs that we add to our methods
     public static void main(String[] args){
-        System.out.println("Hello world. My secret number is: " + addNumbers(1, 2) + ".");
+        int num = 10;
+        //System.out.println("Hello world. My secret number is: " + addNumbers(1, 2) + ".");
+        spyName(num);
     }
 
     //void = returns nothing
     //int method return integers
     public static int addNumbers(int x, int y){
         return x + y;
+    }
+
+    public static void spyName(int x){
+        int counter = 0;
+
+        while(x != counter){
+            System.out.println("Bob " + counter);
+            counter++;
+        }
     }
 
     /*
