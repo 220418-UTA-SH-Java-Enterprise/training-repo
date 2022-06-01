181952977a62511cfa3e365f1829ed0917c02e28
Tiffany Obi <tiffanyobi123@gmail.com>
2022-06-01 12:26:07 -0400

    tour of heroes- angular app demo

---
+++ demos/7 - Angular/TourOfHeros/angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  // hero = 'Windstorm';
+
+  // hero: Hero = {
+  //   id:1,
+  //   name:'Windstorm'
+  // };
+
+  heroes = HEROES;
+
+  selectedHero?:Hero;
+
+  onSelect(hero:Hero):void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
