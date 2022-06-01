2fde90425b3e438af768c2402053365b4b2cc91b
Tiffany Obi <tiffanyobi123@gmail.com>
2022-06-01 14:07:20 -0400

    added message component

--- demos/7 - Angular/TourOfHeros/angular-tour-of-heroes/src/app/app.module.ts
+++ demos/7 - Angular/TourOfHeros/angular-tour-of-heroes/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
